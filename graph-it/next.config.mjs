97539c33c1419f348f72164dfa657e22e62364cf
RodolphoStraub <89853868+RodolphoStraub@users.noreply.github.com>
2024-09-04 18:04:12 -0300

    correções

--- graph-it/next.config.mjs
+++ graph-it/next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export',
-};
-
-export default nextConfig;
+import { PHASE_DEVELOPMENT_SERVER } from 'next/constants.js'
+export default (phase) => {
+    const isDev = phase === PHASE_DEVELOPMENT_SERVER
+    /**
+     * @type {import('next').NextConfig}
+     */
+    const nextConfig = {
+      assetPrefix: isDev ? undefined : 'https://rodolphostraub.github.io.com',
+    }
+    return nextConfig
+  }
