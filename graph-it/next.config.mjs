80f22f384c82808d45f0cbcc8834355364212bb1
RodolphoStraub <89853868+RodolphoStraub@users.noreply.github.com>
2024-09-05 17:31:39 -0300

    update gitignore and next.config.js

--- graph-it/next.config.mjs
+++ graph-it/next.config.mjs
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
-import { PHASE_DEVELOPMENT_SERVER } from 'next/constants.js'
-export default (phase) => {
-    const isDev = phase === PHASE_DEVELOPMENT_SERVER
-    /**
-     * @type {import('next').NextConfig}
-     */
-    const nextConfig = {
-      assetPrefix: isDev ? undefined : 'https://rodolphostraub.github.io.com',
-    }
-    return nextConfig
+const nextConfig = {
+  output: 'export',
+  distDir: 'dist',
+  images: {
+      unoptimized: true,
   }
+};
+
+export default nextConfig;
